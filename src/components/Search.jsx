b26e0f24ef24a7b9b64d82d509074ef44d1818b5
Gurmukh Kharod <60812185+GurmukhSKharod@users.noreply.github.com>
2024-12-30 02:36:56 -0800

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -22,7 +22,7 @@ export const Search = () => {
         placeholder="🔎 Search Google or type URL"
         onChange={(e) => setText(e.target.value)}
       />
-      {!text && (
+      {text && (
         <button type="button" className="absolute top-1.5 right-4 text-2xl text-gray-500 " onClick={() => setText('')}>
           x
         </button>
