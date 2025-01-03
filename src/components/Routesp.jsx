09826770e6d643daa303c640780a68429e622a83
Gurmukh Kharod <60812185+GurmukhSKharod@users.noreply.github.com>
2025-01-03 02:34:31 -0800

    Update Routesp.jsx

--- src/components/Routesp.jsx
+++ src/components/Routesp.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Results } from './Results';
 
 export const Routesp = () => (
   <div className="p-4">
-    <Navigate from="/" to="/search" />
     <Routes>
+      <Route path="/" element={<Navigate to="/search" />} />
       <Route path="/search" element={<Results />} />
+      <Route path="/images" element={<Results />} />
+      <Route path="/news" element={<Results />} />
+      <Route path="/videos" element={<Results />} />
     </Routes>
   </div>
 );
