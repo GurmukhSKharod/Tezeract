5ee6e4f0b0d128eab61d1f1912e0e981d7b3ff78
Gurmukh Kharod <60812185+GurmukhSKharod@users.noreply.github.com>
2025-01-03 02:18:50 -0800

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useDebounce } from 'use-debounce';
-
 import { useResultContext } from '../contexts/ResultContextProvider';
-import { Links } from './Links';
 
 export const Search = () => {
-  const [text, setText] = useState('Software Development');
+  const [text, setText] = useState('');
   const { setSearchTerm } = useResultContext();
   const [debouncedValue] = useDebounce(text, 300);
 
@@ -16,20 +14,23 @@ export const Search = () => {
   }, [debouncedValue, setSearchTerm]);
 
   return (
-    <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
+    <div className="relative sm:ml-48 md:ml-72 sm:mt-10 mt-3">
       <input
         value={text}
         type="text"
         className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
-        placeholder="🔎 Search Google or type URL"
+        placeholder="Search Tezeract 🔎"
         onChange={(e) => setText(e.target.value)}
       />
-      {!text && (
-        <button type="button" className="absolute top-1.5 right-4 text-2xl text-gray-500" onClick={() => setText('')}>
-          x
+      {text && (
+        <button
+          type="button"
+          className="absolute top-1.5 right-4 text-2xl text-gray-500"
+          onClick={() => setText('')}
+        >
+          ✖
         </button>
       )}
-      <Links />
     </div>
   );
 };
