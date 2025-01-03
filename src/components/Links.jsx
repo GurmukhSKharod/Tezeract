61b3ef5ff1aaa33e40f20cf115f74b6f914ce46d
Gurmukh Kharod <60812185+GurmukhSKharod@users.noreply.github.com>
2025-01-03 02:27:17 -0800

    Update Links.jsx

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -5,15 +5,22 @@ const links = [
   { url: '/search', text: '🔎 All' },
   { url: '/news', text: '📰 News' },
   { url: '/images', text: '📸 Images' },
-  { url: '/videos', text: '📺 Videos' },
+  { url: '/videos', text: '🎥 Videos' },
 ];
 
 export const Links = () => {
-    return (
-        <div className="flex sm:justify-around justify-between items-center mt-4">
-            {links.map(({ url, text }) => (
-            <NavLink to={url} className="m-2 mb-0" activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">{text}</NavLink>
-            ))}
-        </div>
-    );
-}
+  return (
+    <div className="flex sm:justify-around justify-between items-center mt-4">
+      {links.map(({ url, text }) => (
+        <NavLink
+          key={url}
+          to={url}
+          activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2"
+          className="m-2 mb-0"
+        >
+          {text}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
